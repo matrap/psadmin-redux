9e5883949fcff1f3b7266c77c5027fb0d97c2fd4
Mateusz Rapacz <mateusz.rapacz@comarch.pl>
2017-07-11 17:38:54 +0200

    Redux flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,13 +21,19 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        this.props.dispatch(courseActions.createCourse(this.state.course))
+        this.props.dispatch(courseActions.createCourse(this.state.course));
     }    
 
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
+
     render() {
+        debugger;
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input 
                 type="text"
@@ -42,10 +48,22 @@ class CoursesPage extends React.Component {
     }
 }
 
+CoursesPage.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+};
+
 function mapStateToProps(state, ownProps) {
+    debugger;
     return {
         courses: state.courses
     }; 
 }
 
-export default connect(mapStateToprops)(CoursesPage);
+/*function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    };
+}*/
+
+export default connect(mapStateToProps)(CoursesPage);
