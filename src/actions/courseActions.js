d4b14c0a63437e83c6a01aef5bafa8545aa17cd1
Mateusz Rapacz <mateusz.rapacz@comarch.pl>
2017-08-11 18:31:10 +0200

    Add ManageCoursePage.js

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -2,7 +2,7 @@ import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
 export function loadCoursesSuccess(courses) {
-    return { type: types.LOAD_COURSES_CUCCES, courses };
+    return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
 export function loadCourses() {
@@ -12,7 +12,7 @@ export function loadCourses() {
                 dispatch(loadCoursesSuccess(courses));
             })
             .catch(error => {
-                //throw(error);
+                throw(error);
             });
     };
 }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Router, browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import routes from './routes';
 import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
     <Provider store={store}>
