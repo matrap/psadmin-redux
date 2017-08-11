7ce9343befb5583c17ce088d1f76efd1f4081f91
Mateusz Rapacz <mateusz.rapacz@comarch.pl>
2017-08-11 13:15:27 +0200

    Get courses from api (using thunk)

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import {Provider} from 'react-redux';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/styles.css'; // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
 
-
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
